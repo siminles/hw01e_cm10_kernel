d4ab6ee8a97bc87a0c403f4d8f43b3ed598825ca
simin <1813765241@qq.com>
2014-05-24 16:03:22 +0800

    fix the mpdecision

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -48,7 +48,7 @@
 #define MSM_MPDEC_STARTDELAY            20000
 #define MSM_MPDEC_DELAY                 130
 #define MSM_MPDEC_PAUSE                 10000
-#define MSM_MPDEC_IDLE_FREQ             486000
+#define MSM_MPDEC_IDLE_FREQ             384000
 #ifdef CONFIG_MSM_MPDEC_INPUTBOOST_CPUMIN
 #define MSM_MPDEC_BOOSTTIME             1000
 #define MSM_MPDEC_BOOSTFREQ_CPU0        918000
@@ -127,8 +127,8 @@ static struct msm_mpdec_tuners {
 #endif
 };
 
-static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
-static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
+static unsigned int NwNs_Threshold[8] = {12, 0, 0, 8, 0, 0, 0, 0};
+static unsigned int TwTs_Threshold[8] = {120, 0, 0, 250, 0, 0, 0, 0};
 
 extern unsigned int get_rq_info(void);
 extern unsigned long acpuclk_get_rate(int);
