3ed73dc5ee29ce582fa70fc7d04a2689832c4f2c
simin <1813765241@qq.com>
2014-05-27 00:51:33 +0800

    upto the bus'mbps & mdp clk+fix some vdd & sync the cpu'clock with L2'clock

--- arch/arm/mach-msm/board-8960-display.c
+++ arch/arm/mach-msm/board-8960-display.c
@@ -557,8 +557,8 @@ static struct msm_bus_vectors mdp_ui_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 216000000 * 2,
-		.ib = 270000000 * 2,
+		.ab = 230400000 * 2, /*216000000 * 2,*/
+		.ib = 288000000 * 2, /*270000000 * 2,*/
 	},
 };
 
@@ -577,8 +577,8 @@ static struct msm_bus_vectors mdp_720p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 230400000 * 2,
-		.ib = 288000000 * 2,
+		.ab = 334080000 * 2, /*.ab = 230400000 * 2,*/
+		.ib = 417600000 * 2, /*.ib = 288000000 * 2,*/
 	},
 };
 
@@ -629,7 +629,7 @@ static struct msm_bus_scale_pdata mdp_bus_scale_pdata = {
 
 static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = MDP_VSYNC_GPIO,
-	.mdp_max_clk = 200000000,
+	.mdp_max_clk = 266667000,
 #ifdef CONFIG_MSM_BUS_SCALING
 	.mdp_bus_scale_table = &mdp_bus_scale_pdata,
 #endif
