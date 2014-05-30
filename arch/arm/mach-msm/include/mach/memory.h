49f258cca6c484eefe87ac8ca953cade85437c07
simin <1813765241@qq.com>
2014-05-30 19:40:44 +0800

    Enable caching and buffering for all of physical RAM in the decompres

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -18,6 +18,9 @@
 #include <linux/types.h>
 
 /* physical offset of RAM */
+#ifdef CONFIG_HAVE_END_MEM
+#define END_MEM		 UL(CONFIG_END_MEM)
+#endif
 #define PLAT_PHYS_OFFSET UL(CONFIG_PHYS_OFFSET)
 
 #define MAX_PHYSMEM_BITS 32
