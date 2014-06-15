7e0b94f023306c58f3a7bfa733cb24557af70b3a
simin <1813765241@qq.com>
2014-06-15 11:04:40 +0800

    topology: Squashed commit of patches

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -1,10 +1,12 @@
 #ifndef _ASM_ARM_TOPOLOGY_H
 #define _ASM_ARM_TOPOLOGY_H
+
 #ifdef CONFIG_ARM_CPU_TOPOLOGY
 
 #include <linux/cpumask.h>
 
 struct cputopo_arm {
+	int id;
 	int thread_id;
 	int core_id;
 	int socket_id;
@@ -26,14 +28,23 @@ void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
-void set_power_scale(unsigned int cpu, unsigned long power);
+
+int topology_register_notifier(struct notifier_block *nb);
+int topology_unregister_notifier(struct notifier_block *nb);
+
 #else
 
 static inline void init_cpu_topology(void) { }
 static inline void store_cpu_topology(unsigned int cpuid) { }
 
-static inline void set_power_scale(unsigned int cpu, unsigned long power) { }
+static inline void set_power_scale(unsigned int cpu, unsigned int power) { }
+static inline int topology_register_notifier(struct notifier_block *nb)  { }
+static inline int topology_unregister_notifier(struct notifier_block *nb)  { }
+
 #endif
+
+/* Topology notifier event */
+#define TOPOLOGY_POSTCHANGE 0
 
 /* Common values for CPUs */
 #ifndef SD_CPU_INIT
@@ -67,6 +78,7 @@ static inline void set_power_scale(unsigned int cpu, unsigned long power) { }
 	.balance_interval	= 1,					\
 }
 #endif
+
 #include <asm-generic/topology.h>
 
 #endif /* _ASM_ARM_TOPOLOGY_H */
